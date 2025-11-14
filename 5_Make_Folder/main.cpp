7aa97961a7707c2dc52ef076da86ad30ef3fc454
MWASHWORTH <matthew.ashworth.42@gmail.com>
2025-11-14 00:26:18 +0000

    Submit attempt 1, Could not run

--- 5_Make_Folder/main.cpp
+++ 5_Make_Folder/main.cpp
@@ -1,18 +1,3 @@
-/*
-Given class Triangle (in files Triangle.h and Triangle.cpp), complete main() to read and set the base and height of triangle1 and of triangle2, determine which triangle's area is smaller, and output that triangle's info, making use of Triangle's relevant member functions.
-
-Ex: If the input is:
-
-3.0 4.0
-4.0 5.0
-where 3.0 is triangle1's base, 4.0 is triangle1's height, 4.0 is triangle2's base, and 5.0 is triangle2's height, the output is:
-
-Triangle with smaller area:
-Base: 3.00
-Height: 4.00
-Area: 6.00
-*/
-
 #include <iostream>
 #include "Triangle.h"
 using namespace std;
@@ -20,15 +5,25 @@ using namespace std;
 int main() {
    Triangle triangle1;
    Triangle triangle2;
+   double base;
+   double height;
 
-   // TODO: Read and set base and height for triangle1 (use SetBase() and SetHeight())
-      
-   // TODO: Read and set base and height for triangle2 (use SetBase() and SetHeight())
-       
+   cin >> base >> height;
+   triangle1.SetBase(base);
+   triangle1.SetHeight(height);
+   
+   cin >> base >> height;
+   triangle2.SetBase(base);
+   triangle2.SetHeight(height);
+
    cout << "Triangle with smaller area:" << endl;
    
-   // TODO: Determine smaller triangle (use GetArea())  
-   //       and output smaller triangle's info (use PrintInfo())
+   if (triangle1.GetArea() < triangle2.GetArea()){
+      triangle1.PrintInfo();
+   }
+   else{
+      triangle2.PrintInfo();
+   }
    
    return 0;
 }
